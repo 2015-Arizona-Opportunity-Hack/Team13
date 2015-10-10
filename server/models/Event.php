6305e179cfbd9bd99a5ba0705f7e586130c84231
developerDemetri <developerdemetri@gmail.com>
2015-10-10 16:29:23 -0700

    added event service files

---
+++ server/models/Event.php
@@ -0,0 +1,36 @@
+<?php 
+	namespace Models;
+
+	use Lib\Database;
+	use Lib\Hash;
+	use Illuminate\Database\Eloquent\Model as Eloquent;
+
+	class Event extends Eloquent {	
+			/**
+	     * The database table used by the model.
+	     *
+	     * @var string
+	     */
+	    protected $table = 'hosts';
+	    /**
+	     * The database table primary key used by the model.
+	     *
+	     * @var string
+	     */
+	    public $primaryKey  = 'id';
+	    /**
+	     * Tells Eloquent that our database table doesn't have timestamps
+	     *
+	     * @var boolean
+	     */
+	    public $timestamps = false; 
+
+		protected $database;
+		
+		function __construct() {
+			$this->database = Database::getInstance();
+		}
+		
+	}
+
+?>
---
+++ server/routes/event.php
@@ -0,0 +1,10 @@
+<?php
+
+	use Models\Host;
+	use Lib\OAuth2\OAuth2;
+
+	$app->group('/event', function() use($app, $authorize, $resourceServer) {
+		
+	});
+
+?>
